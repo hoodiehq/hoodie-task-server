cc13e3c1cf6346322827cbd9c01d4c736f258e07
Dale Shaw <dale@xiatron.com>
2016-12-26 17:19:22 -0700

    Clean up spacing.

--- api/index.js
+++ api/index.js
@@ -4,9 +4,9 @@ function TaskAPIFactory (PouchDB) {
   if (typeof PouchDB !== 'function') {
     throw new Error('Task API Factory requires PouchDB as first argument')
   }
-  
+
   var storeApi = require('@hoodie/store-server/api')(PouchDB);  
-  
+
   return storeApi.create('tasks')
   .then(makeApi)
   .catch(function (error) {
@@ -16,32 +16,32 @@ function TaskAPIFactory (PouchDB) {
       return makeApi();
     }
   })
-  
+
   function makeApi() {
     return storeApi.open('tasks')
     .then(function (taskStore) {
       var api = {}
-
+      
       api.adapter = storeApi.adapter
       api.on = taskStore.on
-  
+      
       api.success = function (taskDoc, data) {
         if (!taskDoc) throw new Error('Task Error requires taskdoc as first argument')
         if (data) taskDoc['data'] = data
         taskStore.remove(taskDoc);
       }
-  
+      
       api.error = function(taskDoc, error) {
         if (!taskDoc) throw new Error('Task Error requires taskdoc as first argument')
         if (!error) throw new Error('Task Error requires error as second argument')
         taskDoc['error'] = error
         taskStore.remove(taskDoc)
       }
-
+      
       return api
     })
     .catch(function (error) {
-        throw error
+      throw error
     })
   }
 }
