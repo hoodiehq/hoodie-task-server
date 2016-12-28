3828e21631a984b3bd8ceb793345b19bcbdf96e8
Dale Shaw <dale@xiatron.com>
2016-12-28 08:53:58 -0700

    Now using store-server-api module.

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ function TaskAPIFactory (PouchDB) {
     throw new Error('Task API Factory requires PouchDB as first argument')
   }
 
-  var storeApi = require('@hoodie/store-server/api')(PouchDB);  
+  var storeApi = require('@hoodie/store-server-api')(PouchDB)
 
   return storeApi.create('tasks')
   .then(makeApi)
@@ -13,7 +13,7 @@ function TaskAPIFactory (PouchDB) {
     if (error.status !== 409) {
       throw new Error('Something unexpected happened: '+error)
     } else {
-      return makeApi();
+      return makeApi()
     }
   })
 
@@ -21,23 +21,23 @@ function TaskAPIFactory (PouchDB) {
     return storeApi.open('tasks')
     .then(function (taskStore) {
       var api = {}
-      
+
       api.adapter = storeApi.adapter
       api.on = taskStore.on
-      
+
       api.success = function (taskDoc, data) {
         if (!taskDoc) throw new Error('Task Error requires taskdoc as first argument')
         if (data) taskDoc['data'] = data
         taskStore.remove(taskDoc);
       }
-      
+
       api.error = function(taskDoc, error) {
         if (!taskDoc) throw new Error('Task Error requires taskdoc as first argument')
         if (!error) throw new Error('Task Error requires error as second argument')
         taskDoc['error'] = error
         taskStore.remove(taskDoc)
       }
-      
+
       return api
     })
     .catch(function (error) {
